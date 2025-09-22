643fd4553d6f00af93878507408d3a3daf61c5e0
Marc Bachmann <marc.brookman@gmail.com>
2025-09-22 12:45:49 +0200

    fix(equality): Fix equality and inequality support for dynamic vars

--- test/comparisons.test.js
+++ test/comparisons.test.js
@@ -26,6 +26,51 @@ describe('comparison operators', () => {
     test('should compare unequal booleans', (t) => {
       t.assert.strictEqual(evaluate('true == false'), false)
     })
+
+    test('does not support non-dynamic comparisons', (t) => {
+      const err = /no such overload:/
+      t.assert.throws(() => evaluate('true == null'), err)
+      t.assert.throws(() => evaluate('false == null'), err)
+      t.assert.throws(() => evaluate('1.0 == null'), err)
+      t.assert.throws(() => evaluate('1 == null'), err)
+      t.assert.throws(() => evaluate('true == null'), err)
+      t.assert.throws(() => evaluate('true == 1'), err)
+    })
+
+    test('supports dynamic comparisons', (t) => {
+      const ctx = {
+        v: {
+          true: true,
+          false: false,
+          null: null,
+          int: 1n,
+          double: 1,
+          string: 'hello'
+        }
+      }
+
+      for (const key of Object.keys(ctx.v)) {
+        t.assert.strictEqual(evaluate(`v["${key}"] == ""`, ctx), false)
+        t.assert.strictEqual(evaluate(`v["${key}"] == 999`, ctx), false)
+        t.assert.strictEqual(evaluate(`v["${key}"] == 999.1`, ctx), false)
+        t.assert.strictEqual(evaluate(`v["${key}"] == []`, ctx), false)
+        t.assert.strictEqual(evaluate(`v["${key}"] == {}`, ctx), false)
+        t.assert.strictEqual(evaluate(`"" == v["${key}"]`, ctx), false)
+        t.assert.strictEqual(evaluate(`999 == v["${key}"]`, ctx), false)
+        t.assert.strictEqual(evaluate(`999.1 == v["${key}"]`, ctx), false)
+        t.assert.strictEqual(evaluate(`[] == v["${key}"]`, ctx), false)
+        t.assert.strictEqual(evaluate(`{} == v["${key}"]`, ctx), false)
+      }
+
+      t.assert.strictEqual(evaluate('v["null"] == null', ctx), true)
+      t.assert.strictEqual(evaluate('v["true"] == true', ctx), true)
+      t.assert.strictEqual(evaluate('v["true"] == true', ctx), true)
+      t.assert.strictEqual(evaluate('v["false"] == false', ctx), true)
+      t.assert.strictEqual(evaluate('v["int"] == 1', ctx), true)
+      t.assert.strictEqual(evaluate('v["double"] == 1.0', ctx), true)
+      t.assert.strictEqual(evaluate('v["double"] == 1', ctx), true)
+      t.assert.strictEqual(evaluate('v["null"] == null', ctx), true)
+    })
   })
 
   describe('inequality', () => {
@@ -35,6 +80,51 @@ describe('comparison operators', () => {
 
     test('should compare equal numbers', (t) => {
       t.assert.strictEqual(evaluate('1 != 1'), false)
+    })
+
+    test('does not support non-dynamic comparisons', (t) => {
+      const err = /no such overload:/
+      t.assert.throws(() => evaluate('true != null'), err)
+      t.assert.throws(() => evaluate('false != null'), err)
+      t.assert.throws(() => evaluate('1.0 != null'), err)
+      t.assert.throws(() => evaluate('1 != null'), err)
+      t.assert.throws(() => evaluate('true != null'), err)
+      t.assert.throws(() => evaluate('true != 1'), err)
+    })
+
+    test('supports dynamic comparisons', (t) => {
+      const ctx = {
+        v: {
+          true: true,
+          false: false,
+          null: null,
+          int: 1n,
+          double: 1,
+          string: 'hello'
+        }
+      }
+
+      for (const key of Object.keys(ctx.v)) {
+        t.assert.strictEqual(evaluate(`v["${key}"] != ""`, ctx), true)
+        t.assert.strictEqual(evaluate(`v["${key}"] != 999`, ctx), true)
+        t.assert.strictEqual(evaluate(`v["${key}"] != 999.1`, ctx), true)
+        t.assert.strictEqual(evaluate(`v["${key}"] != []`, ctx), true)
+        t.assert.strictEqual(evaluate(`v["${key}"] != {}`, ctx), true)
+        t.assert.strictEqual(evaluate(`"" != v["${key}"]`, ctx), true)
+        t.assert.strictEqual(evaluate(`999 != v["${key}"]`, ctx), true)
+        t.assert.strictEqual(evaluate(`999.1 != v["${key}"]`, ctx), true)
+        t.assert.strictEqual(evaluate(`[] != v["${key}"]`, ctx), true)
+        t.assert.strictEqual(evaluate(`{} != v["${key}"]`, ctx), true)
+      }
+
+      t.assert.strictEqual(evaluate('v["null"] != null', ctx), false)
+      t.assert.strictEqual(evaluate('v["true"] != true', ctx), false)
+      t.assert.strictEqual(evaluate('v["true"] != true', ctx), false)
+      t.assert.strictEqual(evaluate('v["false"] != false', ctx), false)
+      t.assert.strictEqual(evaluate('v["int"] != 1', ctx), false)
+      t.assert.strictEqual(evaluate('v["double"] != 1.0', ctx), false)
+      t.assert.strictEqual(evaluate('v["double"] != 1', ctx), false)
+      t.assert.strictEqual(evaluate('v["null"] != null', ctx), false)
     })
   })
 
